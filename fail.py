996fdb3a5d11847e92fc1c177ff02f07df3b3fe5
lh1808 <luka.hauck@web.de>
2026-04-13 16:15:56 +0200

    Update fail.py

--- fail.py
+++ fail.py
@@ -1,13 +1,25 @@
-Noch besser:
+ WARN Encountered 1 warning while parsing the manifest:
+  ⚠ The `project` field is deprecated. Use `workspace` instead.
+    ╭─[/mnt/rubin/pixi.toml:1:1]
+  1 │ ╭─▶ [project]
+  2 │ │   name = "rubin"
+  3 │ │   description = "Causal ML Framework – Analyse- und Production-Pipelines"
+  4 │ │   channels = [
+  5 │ │     "https://nexus3.lan.huk-coburg.de/repository/conda-qc-huk",
+  6 │ │     "https://nexus3.lan.huk-coburg.de/repository/conda-forge",
+  7 │ │   ]
+  8 │ │   platforms = ["linux-64"]
+  9 │ ├─▶ conda-pypi-map = {"https://nexus3.lan.huk-coburg.de/repository/conda-forge" = "https://nexus.lan.huk-coburg.de/repository/raw-githubusercontent/prefix-dev/parselmouth/refs/heads/main/files/compressed_mapping.json"}
+    · ╰──── replace this with 'workspace'
+ 10 │     
+    ╰────
 
-[project]
-conda-pypi-map = {"https://nexus3.lan.huk-coburg.de/repository/conda-forge" = "https://nexus.lan.huk-coburg.de/repository/raw-githubusercontent/prefix-dev/parselmouth/refs/heads/main/files/compressed_mapping.json"}
-
-[pypi-options]
-index-url = "https://nexus.lan.huk-coburg.de/repository/pypi/simple"
-und env variable
-
-PIXI_TLS_ROOT_CERTS="all"
-Dann
-
-pixi add --pypi spaneval
+  ⠈ default:linux-64     [00:00:02] resolving pyyaml==6.0.3                              Error:   × failed to solve the pypi requirements of environment 'default' for platform 'linux-
+  │ 64'
+  ├─▶ failed to resolve pypi dependencies
+  ├─▶ Failed to fetch: `https://nexus.lan.huk-coburg.de/repository/pypi/simple/flaml/`
+  ├─▶ Request failed after 3 retries
+  ├─▶ error sending request for url (https://nexus.lan.huk-coburg.de/repository/pypi/
+  │   simple/flaml/)
+  ├─▶ client error (Connect)
+  ╰─▶ invalid peer certificate: UnknownIssuer
