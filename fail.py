f41ab51e7eea89432c11037b2d3d24f990ad4c3d
lh1808 <luka.hauck@web.de>
2026-04-22 13:07:09 +0200

    Update fail.py

--- fail.py
+++ fail.py
@@ -1,35 +1,26 @@
-Der Termineingang für Erstellung der Prognose in PLUTO soll in folgender Granularität übertragen werden:
-
-Die Prognose soll für die beiden Kennzahlen "Termineingang Schriftstücke" (TERM_EINGANG_SCHRIFTST) und "Termineingang Sonstige Termine" (TERM_EINGANG_SONST) ermittelt werden. 
-
-Die beiden Kennzahlen werden nach folgenden Produkten unterschieden:
-
-KFZ_Vollkasko
-KFZ_Teilkasko
-KFZ_Haftpflicht
-KFZ_Rest
-HUS_Haftpflicht
-HUS_Wohngebäude
-HUS_Hausrat
-HUS_Rest
-Für den Termineingang pro Produkt wird zusätzlich die Information zum Schadenstatus "Neuschaden" und "Folgebearbeitung" benötigt. 
-
- 
-
-SQL --> Connection:
-
-
-
-SQL --> Abfrage:
-
-SELECT DIM_ZEIT, DIM_KENNZAHL,DIM_PRODUKT, DIM_SCHADENSTATUS, KENNZAHLWERT FROM t7.TA_DA_PLUTO_SP_2025
-WHERE DIM_KENNZAHL IN ('TERM_EINGANG_SCHRIFTST', 'TERM_EINGANG_SONST')
-
-Aufbau Schreibtabelle TA_DA_PLUTO_SP_2025_PROGNOSE:
-
-DIM_ZEIT als Integer (Datum)
-DIM_KENNZAHL = 'TERM_EINGANG_SCHRIFTST' und 'TERM_EINGANG_SONST'
-DIM_PRODUKT als VARCHAR(50)
-DIM_SCHADENSTATUS als VARCHAR(50)
-KENNZAHLWERT als DECIMAL(20,9)
-LOAD_DATE als Date (Ladedatum)
+WARNING:pluto_multivariate_repository:UNERWARTETE PRODUKTE in DB gefunden (nicht in product_list): ['HUS_Eingabe', 'KFZ_Kasko_Eingabe']. Diese werden trotzdem verarbeitet.
+WARNING:pluto_multivariate_repository:UNERWARTETER SCHADENSTATUS in DB gefunden (nicht in status_list): ['LFD', 'NEU']. Diese werden trotzdem verarbeitet.
+WARNING:pluto_multivariate_repository:FEHLENDER SCHADENSTATUS: In der erwarteten Liste, aber nicht in der DB vorhanden: ['Folgebearbeitung', 'Neuschaden'].
+INFO:pluto_multivariate_repository:DB2 connection successfully closed.
+Traceback (most recent call last):
+  File "/mnt/da-pluto-timeseries/pluto_forecast_job.py", line 235, in <module>
+    run_pluto_multivariate_forecast_job()
+  File "/mnt/da-pluto-timeseries/pluto_forecast_job.py", line 154, in run_pluto_multivariate_forecast_job
+    results = run_full_job(df_daily, cfg=cfg, logger=None)
+              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/mnt/da-pluto-timeseries/forecasting/pipeline.py", line 596, in run_full_job
+    artifacts, metrics_df, backtest, true_ts = train_and_evaluate_for_horizon(
+                                               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/mnt/da-pluto-timeseries/forecasting/pipeline.py", line 408, in train_and_evaluate_for_horizon
+    res = rolling_block_forecast(
+          ^^^^^^^^^^^^^^^^^^^^^^^
+  File "/mnt/da-pluto-timeseries/forecasting/utils.py", line 268, in rolling_block_forecast
+    model = model_builder()
+            ^^^^^^^^^^^^^^^
+  File "/mnt/da-pluto-timeseries/forecasting/pipeline.py", line 401, in model_builder_eval
+    return build_tft(hcfg, effective_tft_cfg)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/mnt/da-pluto-timeseries/forecasting/model.py", line 403, in build_tft
+    if tft_cfg.weight_decay > 0:
+       ^^^^^^^^^^^^^^^^^^^^^^^^
+TypeError: '>' not supported between instances of 'str' and 'int'
