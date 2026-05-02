43947ef7faf7ca14a3aa473b4bc130153e2012ed
lh1808 <luka.hauck@web.de>
2026-05-02 22:22:36 +0200

    Update fail.py

--- fail.py
+++ fail.py
@@ -1,38 +1,17 @@
-(generic) ubuntu@192.168.5.216 ~/rubin $ pixi run python -c "
-import ctypes, os
-try:
-    cuda = ctypes.CDLL('libcuda.so.1')
-    print('libcuda.so.1 geladen')
-except: print('libcuda.so.1 NICHT gefunden')
+# 1. Conda-CatBoost + CUDA 12.9 runtime entfernen
+pixi remove catboost cuda-cudart cuda-cudart_linux-64 cuda-version
 
-# CatBoost's interne CUDA-Version
-from catboost.utils import get_gpu_device_count
-print('GPU count:', get_gpu_device_count())
+# 2. CatBoost von PyPI über pixi installieren (statisch gelinkte CUDA)
+pixi add --pypi catboost==1.2.10
+
+# 3. Verifizieren: Muss OHNE cuda129 im Build-String sein
+pixi run python -c "import catboost; print(catboost.__version__, catboost.__file__)"
+
+# 4. GPU testen
+pixi run python -c "
+from catboost import CatBoostRegressor
+import numpy as np
+m = CatBoostRegressor(iterations=5, task_type='GPU', verbose=1, allow_writing_files=False)
+m.fit(np.random.rand(100,5).astype(np.float32), np.random.rand(100))
+print('GPU OK!')
 "
-libcuda.so.1 geladen
-GPU count: 1
-(generic) ubuntu@192.168.5.216 ~/rubin $ pixi list | grep -i cuda
-catboost                            1.2.10        cuda129_py312h3fcdad1_100    44.61 MiB  conda  https://nexus3.lan.huk-coburg.de/repository/conda-forge
-cuda-cudart                         12.9.79       h5888daf_0                   22.70 KiB  conda  https://nexus3.lan.huk-coburg.de/repository/conda-forge
-cuda-cudart_linux-64                12.9.79       h3f2d84a_0                  192.63 KiB  conda  https://nexus3.lan.huk-coburg.de/repository/conda-forge
-cuda-version                        12.9          h4f385c5_3                   21.07 KiB  conda  https://nexus3.lan.huk-coburg.de/repository/conda-forge
-(generic) ubuntu@192.168.5.216 ~/rubin $ pixi run env | grep -i LD_LIBRARY_PATH
-JAVA_LD_LIBRARY_PATH=/opt/conda/envs/generic/lib/jvm/lib/server
-JAVA_LD_LIBRARY_PATH_BACKUP=/opt/conda/envs/generic/lib/jvm/lib/server
-LD_LIBRARY_PATH=/opt/oracle/instantclient_21_6
-(generic) ubuntu@192.168.5.216 ~/rubin $ pixi run python -c "
-import catboost._catboost as cb
-import subprocess
-pid = str(subprocess.os.getpid())
-result = subprocess.run(['cat', f'/proc/{pid}/maps'], capture_output=True, text=True)
-for line in result.stdout.split('\n'):
-    if 'cuda' in line.lower() or 'nvidia' in line.lower():
-        print(line.split()[-1] if len(line.split()) > 5 else line)
-"
-/mnt/rubin/.pixi/envs/default/targets/x86_64-linux/lib/libcudart.so.12.9.79
-/mnt/rubin/.pixi/envs/default/targets/x86_64-linux/lib/libcudart.so.12.9.79
-/mnt/rubin/.pixi/envs/default/targets/x86_64-linux/lib/libcudart.so.12.9.79
-/mnt/rubin/.pixi/envs/default/targets/x86_64-linux/lib/libcudart.so.12.9.79
-/mnt/rubin/.pixi/envs/default/targets/x86_64-linux/lib/libcudart.so.12.9.79
-/mnt/rubin/.pixi/envs/default/lib/libicudata.so.78.3
-/mnt/rubin/.pixi/envs/default/lib/libicudata.so.78.3
