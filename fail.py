245b82abe689cf7a7886802340774cfe5dfc0115
lh1808 <luka.hauck@web.de>
2026-05-18 10:25:20 +0200

    Update fail.py

--- fail.py
+++ fail.py
@@ -1,4 +1,6 @@
-(generic) ubuntu@192.168.150.149 /home/ubuntu/da-hf1-rubin (master) $ pixi run app
-✨ Pixi task (app in app): bash app.sh: (rubin-UI starten (lokal: Port 8501, Domino: $DOMINO_APP_PORT))
-app.sh: line 9: $'\r': command not found
-: invalid option name pipefail
+* text=auto
+
+*.{diff,patch} binary
+
+*.{py,yaml,yml,sh} text eol=lf
+*.bat text eol=crlf
