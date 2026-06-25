1d2348941e3698f89264711867aecbd9cd4e43a8
lh1808 <luka.hauck@web.de>
2026-06-25 12:31:15 +0200

    Update fail.py

--- fail.py
+++ fail.py
@@ -1,4 +1,7 @@
+import numpy as np
 from econml.score import EnsembleCateEstimator
+from rubin.training import _predict_effect
 
+pipe = ProductionPipeline(bundle_path)
 members = [m for n, m in pipe.models.items() if n not in ("Ensemble", "SurrogateTree")]
 ens = EnsembleCateEstimator(cate_models=members, weights=np.ones(len(members)) / len(members))
